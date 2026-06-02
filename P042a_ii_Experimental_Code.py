9daa85f9976db0e233d2ce7ca0e4d36076d5e306
marisacalarco <calarcomarisa@gmail.com>
2026-06-02 12:12:06 -0700

    Add files via upload

--- P042a_ii_Experimental_Code.py
+++ P042a_ii_Experimental_Code.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Created on Fri Apr  3 11:29:48 2026
--- P042a_iii_Experimental_Code.py
+++ P042a_iii_Experimental_Code.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Created on Thu May 28 16:41:47 2026
